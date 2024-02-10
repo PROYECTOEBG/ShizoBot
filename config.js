bdf298024af5d7620f18c1417477b010c416dc61
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-10 18:59:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,46 +1,51 @@
-import { watchFile, unwatchFile } from 'fs' 
+import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
-import moment from 'moment-timezone' 
-import fs from 'fs' 
 
-//OwnerShip
 global.owner = [
-  ['593993370003', 'Jeffo ❤️✨', true],
-  ['593993370003', 'S.AI Bot', true],
-  ['593993370003', 'Mr. Lens Lord 📸']
-]
-global.mods = []
-global.prems = []
+  ['593993370003', 'FG98', true],
+  ['5491168352204']
+] //Numeros de owner 
 
-global.author = 'Shizo The Techie'
-global.botname = 'ShizoBot'
- 
- 
- //Api's
-global.APIs = {
-  shizoapi: 'https://shizoapi.onrender.com'
+global.mods = [''] 
+global.prems = ['50489079501', '573143917092']
+global.APIs = { // API Prefix
+  // name: 'https://website' 
+  nrtm: 'https://fg-nrtm.ddns.net',
+  fgmods: 'https://api.fgmods.xyz'
 }
-global.APIKeys = { 
-  'https://shizoapi.onrender.com': 'shizo'
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.fgmods.xyz': 'DRLg5kY7' //--- 100 de límite diario --- Regístrese en https://api.fgmods.xyz/
 }
 
-//Apikeys
-global.shizokeys = 'shizo'
+// Sticker WM
+global.packname = 'Senna┃ᴮᴼᵀ' 
+global.author = '@fg98' 
 
-//Sticker Watermarks
-global.stkpack = 'ShizoBot 🥵'
-global.stkowner = '© Shizo The Techie'
+//--info FG
+global.botName = 'Senna'
+global.fgig = 'https://www.instagram.com/fg98_ff' 
+global.fgsc = 'https://github.com/FG98F/dylux-fg' 
+global.fgyt = 'https://youtube.com/fg98f'
+global.fgpyp = 'https://paypal.me/fg98f'
+global.fglog = 'https://i.ibb.co/1zdz2j3/logo.jpgs' 
 
-//management
-global.bug = '*!! Sorry 💢 !!*\nSomething went wrong 🌋'
-global.stop = '*!! 🎭 Unfortunately 💔 !!*\nBot system is not Responding 🙃'
+//--- Grupos WA
+global.fgcanal = 'https://whatsapp.com/channel/0029VaCeuZd6mYPQiWqxXj1F'
+global.bgp = 'https://chat.whatsapp.com/BESBo5xjvIZE4YVvth6Yzr'
+global.bgp2 = 'https://chat.whatsapp.com/I7bvd8XCAOUHjgkHteqFC7'
+global.bgp3 = 'https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH' //--GP NSFW
 
-//TimeLines
-global.botdate = `*⫹⫺ Date:*  ${moment.tz('Asia/Kolkata').format('DD/MM/YY')}`
-global.bottime = `*⫹⫺ Time:* ${moment.tz('Asia/Kolkata').format('HH:mm:ss')}`
+global.wait = '⌛ _Cargando..._\n*▬▬▬▭*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.xmoji = '🔥' 
 
-
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
